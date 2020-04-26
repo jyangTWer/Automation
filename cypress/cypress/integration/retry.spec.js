728b86788ab08c7363707eba585a347ad3552296
Jing Yang <yang.jing@rea-group.com>
2020-04-26 11:11:46 +0800

    Cypress retry 2

--- cypress/cypress/integration/retry.spec.js
+++ cypress/cypress/integration/retry.spec.js
@@ -1,10 +1,13 @@
 describe("retry test", () => {
   it("Can retry the fist command", () => {
     cy.visit('/');
-    cy.get('#random-number') // <div>🎁</div>
-      .invoke('text')        // "🎁"
-      .then(parseFloat)      // NaN
-      .should('be.gte', 1)   // fails
-      .and('be.lte', 10)     // never evaluates
+    cy.get('#random-number')
+      .should(($div) => {
+        // all the code inside here will retry
+        // until it passes or times out
+        const n = parseFloat($div.text())
+
+        expect(n).to.be.gte(1).and.be.lte(10)
+      })
   });
 });
